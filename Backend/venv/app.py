be38d73776ecf8efee7b1cc4b424985e4e978ebe
Deepan Andrew <deepanandrew1@gmail.com>
2026-01-07 23:10:46 +0530

    Day 4: Added POST and GET users API with FastAPI

--- Backend/venv/app.py
+++ Backend/venv/app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app=FastAPI()
+
+users =[]
+
+class User(BaseModel):
+    name:str
+    email:str
 
 @app.get("/")
 def root():
@@ -9,6 +16,15 @@ def root():
 @app.get("/health")
 def health():
     return{"health":"ok"}
+
+@app.post("/users")
+def create_user(user: User):
+    users.append(user)
+    return{"message":"User added successfully","total_users": len(users)}
+    
+@app.get("/users")
+def get_users():
+    return users
 
 @app.get("/hello/{name}")
 def say_hello(name: str):
